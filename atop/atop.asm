2a955c66830dde3f239d790ee2b9cff482560d4b
hexagonix <felipemiguel_nery@hotmail.com>
2021-12-13 00:48:33 -0300

    Alteração no nome do sistema
    Alteração no nome do sistema de Andromeda para Hexagonix.

--- atop/atop.asm
+++ atop/atop.asm
@@ -387,7 +387,7 @@ parametro: dd ?
 
 atop:
 
-.inicio:              db "Visualizador de processos do Sistema Operacional Andromeda(R)", 10, 10, 0   
+.inicio:              db "Visualizador de processos do Sistema Operacional Hexagonix(R)", 10, 10, 0   
 .pid:                 db "PID deste processo: ", 0
 .usoMem:              db 10, 10, "Uso de memoria: ", 0
 .memTotal:            db 10, "Total de memoria instalada identificada: ", 0
@@ -395,7 +395,7 @@ atop:
 .kbytes:              db " kbytes.", 0
 .mbytes:              db " megabytes.", 0
 .uso:                 db "Uso: atop", 10, 10
-                      db "Exibe os processos carregados na pilha de execucao do Andromeda(R).", 10, 10 
+                      db "Exibe os processos carregados na pilha de execucao do Hexagonix(R).", 10, 10 
                       db "Processos do Kernel sao filtrados e nao exibidos nesta lista.", 10, 10            
                       db "atop versao ", versaoATOP, 10, 10
                       db "Copyright (C) 2020-2021 Felipe Miguel Nery Lunkes", 10
