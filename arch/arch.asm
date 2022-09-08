0cfd2c52e08645bbe86cb4477f8331df7ce94126
Felipe Lunkes <felipenldev@gmail.com>
2022-09-07 21:30:42 -0300

    Informações de licença no cabeçalho

--- arch/arch.asm
+++ arch/arch.asm
@@ -12,6 +12,13 @@
 ;;
 ;;
 ;;************************************************************************************
+;;
+;; Este arquivo é licenciado sob licença BSD-3-Clause. Observe o arquivo de licença para
+;; mais informações sobre seus direitos e deveres ao utilizar qualquer trecho deste arquivo.
+;;
+;; Copyright (C) 2016-2022 Felipe Miguel Nery Lunkes
+;; Todos os direitos reservados.
+;;
 
 use32
 
