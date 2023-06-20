e6655bbed2cb39656f32cfb1fec0d00edee919a1
Felipe Lunkes <felipenldev@gmail.com>
2023-06-20 20:10:46 -0300

    v2.1.5

--- cowsay/cowsay.asm
+++ cowsay/cowsay.asm
@@ -392,13 +392,13 @@ terminar:
 
 ;;************************************************************************************
 
-versaoCOWSAY equ "2.1.4"
+versaoCOWSAY equ "2.1.5"
 
 cowsay:
 
 .uso:
 db 10, "Usage: cowsay [profile] [message]", 10, 10
-db "Amusedly display a message to the user.", 10, 10
+db "Display a message to the user.", 10, 10
 db "You can change the animal or entity profile that displays the message.", 10
 db "This change must be requested BEFORE the message.", 10
 db 'In the case of a sentence, the character " must appear before and after the sentence.', 10, 10
