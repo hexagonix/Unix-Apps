406f9693ea343c47ba859fc60823c6d3abfe5bca
Felipe Lunkes <felipenldev@gmail.com>
2023-08-27 20:20:57 -0300

    v1.7.4

--- logind/logind.asm
+++ logind/logind.asm
@@ -102,7 +102,7 @@ tamanhoLimiteBusca = 32768
 
 ;;************************************************************************************
 
-versaoLOGIND equ "1.7.3"
+versaoLOGIND equ "1.7.4"
 
 arquivo:
 db "passwd", 0 ;; Nome do arquivo de configuração de login
@@ -132,7 +132,7 @@ db "  88       88 '8b,   ,aa  ,d8' '8b,  88,    ,88 '8a,   ,d88 '8a,   ,a8' 88  
 db "  88       88  `'Pbbd8'' 8P'     `P8 `'8bbdP'P8  `'PbbdP'P8  `'PbbdP''  88       88 88 8P'     `P8", 10
 db "                                                 aa,    ,88", 10
 db "                                                  'P8bbdP'", 10, 10
-db "                              Hexagonix(R) Operating System", 10, 10
+db "                                Hexagonix Operating System", 10, 10
 db "                  Copyright (C) 2015-", __stringano, " Felipe Miguel Nery Lunkes", 10
 db "                                    All rights reserved.", 10, 0
 
