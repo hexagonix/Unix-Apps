0d2fa70ad38384eca41a49bc6dd4db6783a12644
Felipe Lunkes <felipenldev@gmail.com>
2024-01-06 00:15:45 -0300

    v1.5.0

--- cat/cat.asm
+++ cat/cat.asm
@@ -73,7 +73,7 @@ use32
 include "HAPP.s" ;; Here is a structure for the HAPP header
 
 ;; Instance | Structure | Architecture | Version | Subversion | Entry Point | Image type
-cabecalhoAPP cabecalhoHAPP HAPP.Arquiteturas.i386, 1, 00, applicationStart, 01h
+appHeader headerHAPP HAPP.Architectures.i386, 1, 00, applicationStart, 01h
 
 ;;************************************************************************************
 
@@ -98,14 +98,14 @@ applicationStart:
     mov edi, cat.helpParameter
     mov esi, [parameters]
 
-    hx.syscall compararPalavrasString
+    hx.syscall hx.compareWordsString
 
     jc applicationUsage
 
     mov edi, cat.helpParameter2
     mov esi, [parameters]
 
-    hx.syscall compararPalavrasString
+    hx.syscall hx.compareWordsString
 
     jc applicationUsage
 
@@ -140,7 +140,7 @@ applicationUsage:
 
 finish:
 
-    hx.syscall encerrarProcesso
+    hx.syscall hx.exit
 
 ;;************************************************************************************
 
@@ -150,7 +150,7 @@ finish:
 ;;
 ;;************************************************************************************
 
-VERSION equ "1.4.0"
+VERSION equ "1.5.0"
 
 cat:
 
