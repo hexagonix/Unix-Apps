6cf204d4f22ad0c4ff44cf6a5729dbe7aeb4ee29
Felipe Lunkes <felipenldev@gmail.com>
2025-04-21 16:23:21 -0300

    v0.2.1
    Changing messages and compatibility requirements

--- hash/hash.asm
+++ hash/hash.asm
@@ -73,7 +73,7 @@ use32
 include "HAPP.s" ;; Here is a structure for the HAPP header
 
 ;; Instance | Structure | Architecture | Version | Subversion | Entry Point | Image type
-appHeader headerHAPP HAPP.Architectures.i386, 1, 00, shellStart, 01h
+appHeader headerHAPP HAPP.Architectures.i386, 1, 3, shellStart, 01h
 
 ;;************************************************************************************
 
@@ -643,7 +643,7 @@ finishShell:
 
 ;; TODO: improve shell scripting support
 
-VERSION equ "0.2.0"
+VERSION equ "0.2.1"
 
 searchSizeLimit = 32768
 
