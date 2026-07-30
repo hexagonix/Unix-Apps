1054533fdbca912bfb94af3a47c19fcc42eda1d6
Felipe Lunkes <felipenldev@gmail.com>
2026-07-30 10:31:44 -0300

    v3.1.0: change kernel minimal version

--- ps/ps.asm
+++ ps/ps.asm
@@ -73,7 +73,7 @@ use32
 include "HAPP.s" ;; Here is a structure for the HAPP header
 
 ;; Instance | Structure | Architecture | Version | Subversion | Entry Point | Image type
-appHeader headerHAPP HAPP.Architectures.i386, 1, 00, applicationStart, 01h
+appHeader headerHAPP HAPP.Architectures.i386, 1, 4, applicationStart, 01h
 
 ;;************************************************************************************
 
@@ -348,7 +348,7 @@ parameterOtherProcesses:
 
 ;;************************************************************************************
 
-VERSION equ "3.0.0"
+VERSION equ "3.1.0"
 
 ps:
 
