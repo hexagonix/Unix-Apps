cd898c8c764168bfb0a821e29fb98bae5111c690
Felipe Lunkes <felipenldev@gmail.com>
2026-07-30 10:22:10 -0300

    v0.1.0: add kill Unix  utility

---
+++ kill/kill.asm
@@ -0,0 +1,184 @@
+;;*************************************************************************************************
+;;
+;; 88                                                                                88
+;; 88                                                                                ""
+;; 88
+;; 88,dPPPba,   ,adPPPba, 8b,     ,d8 ,adPPPPba,  ,adPPPb,d8  ,adPPPba,  8b,dPPPba,  88 8b,     ,d8
+;; 88P'    "88 a8P     88  `P8, ,8P'  ""     `P8 a8"    `P88 a8"     "8a 88P'   `"88 88  `P8, ,8P'
+;; 88       88 8PP"""""""    )888(    ,adPPPPP88 8b       88 8b       d8 88       88 88    )888(
+;; 88       88 "8b,   ,aa  ,d8" "8b,  88,    ,88 "8a,   ,d88 "8a,   ,a8" 88       88 88  ,d8" "8b,
+;; 88       88  `"Pbbd8"' 8P'     `P8 `"8bbdP"P8  `"PbbdP"P8  `"PbbdP"'  88       88 88 8P'     `P8
+;;                                               aa,    ,88
+;;                                                "P8bbdP"
+;;
+;;                     Sistema Operacional Hexagonix - Hexagonix Operating System
+;;
+;;                         Copyright (c) 2015-2025 Felipe Miguel Nery Lunkes
+;;                        Todos os direitos reservados - All rights reserved.
+;;
+;;*************************************************************************************************
+;;
+;; Português:
+;;
+;; O Hexagonix e seus componentes são licenciados sob licença BSD-3-Clause. Leia abaixo
+;; a licença que governa este arquivo e verifique a licença de cada repositório para
+;; obter mais informações sobre seus direitos e obrigações ao utilizar e reutilizar
+;; o código deste ou de outros arquivos.
+;;
+;; English:
+;;
+;; Hexagonix and its components are licensed under a BSD-3-Clause license. Read below
+;; the license that governs this file and check each repository's license for
+;; obtain more information about your rights and obligations when using and reusing
+;; the code of this or other files.
+;;
+;;*************************************************************************************************
+;;
+;; BSD 3-Clause License
+;;
+;; Copyright (c) 2015-2025, Felipe Miguel Nery Lunkes
+;; All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+;;
+;; 1. Redistributions of source code must retain the above copyright notice, this
+;;    list of conditions and the following disclaimer.
+;;
+;; 2. Redistributions in binary form must reproduce the above copyright notice,
+;;    this list of conditions and the following disclaimer in the documentation
+;;    and/or other materials provided with the distribution.
+;;
+;; 3. Neither the name of the copyright holder nor the names of its
+;;    contributors may be used to endorse or promote products derived from
+;;    this software without specific prior written permission.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+;; FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+;; CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+;; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;
+;; $HexagonixOS$
+
+use32
+
+;; Now let's create a HAPP header for the application
+
+include "HAPP.s" ;; Here is a structure for the HAPP header
+
+;; Instance | Structure | Architecture | Version | Subversion | Entry Point | Image type
+appHeader headerHAPP HAPP.Architectures.i386, 1, 0, applicationStart, 01h
+
+;;************************************************************************************
+
+include "hexagon.s"
+include "console.s"
+include "macros.s"
+include "errors.s"
+
+;;************************************************************************************
+
+applicationStart:
+
+    mov [parameters], edi
+
+    mov edi, kill.helpParameter
+    mov esi, [parameters]
+
+    hx.syscall hx.compareWordsString
+
+    jc applicationUsage
+
+    mov edi, kill.helpParameter2
+    mov esi, [parameters]
+
+    hx.syscall hx.compareWordsString
+
+    jc applicationUsage
+
+    mov esi, [parameters]
+
+    cmp byte[esi], 0
+    je applicationUsage
+
+    hx.syscall hx.trimString
+
+    hx.syscall hx.stringToInt
+
+    jc invalidPID
+
+    mov ebx, eax
+
+    hx.syscall hx.kill
+
+    jc killFailed
+
+    jmp finish
+
+;;************************************************************************************
+
+killFailed:
+
+    cmp eax, Hexagon.processNotFound
+    je noSuchProcess
+
+    fputs kill.genericError
+
+    jmp finish
+
+noSuchProcess:
+
+    fputs kill.noSuchProcess
+
+    jmp finish
+
+invalidPID:
+
+    fputs kill.invalidPID
+
+    jmp finish
+
+;;************************************************************************************
+
+applicationUsage:
+
+    fputs kill.use
+
+    jmp finish
+
+;;************************************************************************************
+
+finish:
+
+    hx.syscall hx.exit
+
+;;************************************************************************************
+
+VERSION equ "0.1.0"
+
+kill:
+
+.use:
+db 10, "Usage: kill <pid>", 10, 10
+db "Terminate the process identified by <pid>.", 10, 10
+db "kill version ", VERSION, 10, 10
+db "Copyright (C) 2026-", __stringYear, " Felipe Miguel Nery Lunkes", 10
+db "All rights reserved.", 0
+.helpParameter:
+db "?", 0
+.helpParameter2:
+db "--help", 0
+.invalidPID:
+db 10, "kill: invalid PID.", 0
+.noSuchProcess:
+db 10, "kill: no such process.", 0
+.genericError:
+db 10, "kill: unable to terminate the process.", 0
+
+parameters: dd ?
