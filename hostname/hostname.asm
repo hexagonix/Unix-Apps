24e8d23f141dd03d916b19d227c0853f1985d291
Felipe Lunkes <felipenldev@gmail.com>
2026-08-08 01:32:32 -0300

    v1.4.1: update Hexagon version requirements

--- hostname/hostname.asm
+++ hostname/hostname.asm
@@ -73,7 +73,7 @@ use32
 include "HAPP.s" ;; Here is a structure for the HAPP header
 
 ;; Instance | Structure | Architecture | Version | Subversion | Entry Point | Image type
-appHeader headerHAPP HAPP.Architectures.i386, 1, 00, applicationStart, 01h
+appHeader headerHAPP HAPP.Architectures.i386, 1, 7, applicationStart, 01h
 
 ;;************************************************************************************
 
@@ -163,7 +163,7 @@ finish:
 ;;
 ;;************************************************************************************
 
-VERSION equ "1.4.0"
+VERSION equ "1.4.1"
 
 hostname:
 
